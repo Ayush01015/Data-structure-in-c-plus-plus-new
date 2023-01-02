dad505f00d052ab4ed5f7c30a5b989ef3847a00d
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-01-02 20:23:40 +0530

    Delete function added along with driver menu

--- Queue/QueueArray.cpp
+++ Queue/QueueArray.cpp
@@ -6,6 +6,7 @@ class Queue{
         int front, rear;
         int *ptr;
     public:
+        Queue();
         Queue(int);
         void enQueue(int);
         void deQueue();
@@ -13,10 +14,46 @@ class Queue{
         bool isFull();
         int count();
         void view();
+        void peek();
+        ~Queue();
+        void exit();
 };
+void Queue::exit(){
+//
+}
+Queue::~Queue(){
+    delete []ptr;
+}
+Queue::Queue(){
+    capacity = 1;
+    front = -1;
+    rear  = -1;
+    ptr = new int[capacity];
+}
+void Queue::peek(){
+    cout<<"Front: "<<front<<endl;
+    cout<<"Rear: "<<rear<<endl;
+}
 void Queue::view(){
-    for(int i=0;i<capacity;i++)
-        cout<<ptr[i]<<" ";
+    cout<<"Queue---> ";
+    if(front==-1)
+        cout<<"Queue is Empty\n";
+    else if(front<rear){
+        for(int i=front;i<=rear;i++)
+            cout<<ptr[i]<<" ";
+    }
+    else if(front>rear){
+        for(int i=0;i<=rear;i++)
+            cout<<ptr[i]<<" ";
+        for(int j=front;j<=capacity-1;j++)
+            cout<<ptr[j]<<" ";
+    }else if(front==rear){
+        cout<<ptr[front];
+    }
+    else if(capacity-1==rear&&front==0){
+        for(int i=0;i<capacity;i++)
+            cout<<ptr[i]<<" ";
+    }
     cout<<endl;
 }
 Queue::Queue(int cap){
@@ -53,18 +90,49 @@ void Queue::enQueue(int data){
     }
 }
 void Queue::deQueue(){
-    //pending...
+    if(isEmpty())
+        cout<<"Queue is Empty\n";
+    else if(front==rear){
+        front=-1;
+        rear=-1;
+    }else if(capacity-1==front){
+        front=0;
+    }
+    else{
+        front++;
+    }
 }
 int main()
 {
     Queue q(5);
-    q.enQueue(10);
-    q.enQueue(20);
-    q.enQueue(30);
-    q.enQueue(40);
-    q.enQueue(50);
-    q.view();
-    q.deQueue();
-    q.view();
+    while(1){
+        q.view();
+        int choice;
+        cout<<"Choose your Option...\n1.Insert\n2.Delete\n3.isFull\n4.isEmpty\n5.Exit\n";
+        cin>>choice;
+        switch (choice)
+        {
+        case 1:
+            int data;
+            cout<<"Enter Value you wish to enter\n";
+            cin>>data;
+            q.enQueue(data);
+            break;
+        case 2:
+            q.deQueue();
+            break;
+        case 3:
+            cout<<q.isFull()<<endl;
+            break;
+        case 4:
+            cout<<q.isEmpty()<<endl;
+            break;
+        case 5:q.exit();
+            break;
+        default:
+            cout<<"Invalid Choice\n";
+            break;
+        }
+    }
     return 0;
 }
